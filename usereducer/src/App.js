766802239405b609892b491e7abc36523b1174d0
huynvn <Huy.Nguyen@navigosgroup.com>
2023-05-24 18:43:42 +0700

    Update

--- usereducer/src/App.js
+++ usereducer/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
+import "./App.css";
+
+// useReducer
+// 1. Init State
+// 2. Action (Up/Down)
+// 3. Reducer
+// 4. Dispatch
+
+const initState = 0;
+
+const UP_ACTION = "up";
+const DOWN_ACTION = "down";
+
+const reducer = (state, action) => {
+  console.log("reducer running");
+  switch (action) {
+    case UP_ACTION:
+      return state + 1;
+    case DOWN_ACTION:
+      return state - 1;
+    default:
+      throw new Error("Invalid action");
+  }
+};
 
 function App() {
+  // useReducer: có 2 đối số : 1 - reducer , 2 - initState
+  const [count, dispatch] = useReducer(reducer, initState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "0 20px" }}>
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(DOWN_ACTION)}>Down</button>
+      <button onClick={() => dispatch(UP_ACTION)}>Up</button>
     </div>
   );
 }
