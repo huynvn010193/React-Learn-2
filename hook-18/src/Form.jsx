5eb53756e26f8b04435712da7250f2916b332ed1
huynvn <Huy.Nguyen@navigosgroup.com>
2023-12-11 10:58:50 +0700

    Done useTransition

--- hook-18/src/Form.jsx
+++ hook-18/src/Form.jsx
@@ -1,4 +1,4 @@
-import React, { useId, useMemo, useState } from "react";
+import React, { useId, useMemo, useState, useTransition } from "react";
 import StudentList from "./StudentList";
 import studentListData from "./mocks/studentList.json";
 
@@ -6,29 +6,32 @@ const Form = () => {
   // useId():
   const id = useId();
   const [searchInput, setSearchInput] = useState("");
+  const [filterText, setFilterText] = useState("");
+  const [isPeding, startTransition] = useTransition();
 
   // dùng useMemo để ghi nhớ lại giá trị của nó
   const data = useMemo(() => {
     return studentListData.map((student) => {
-      const index = student.indexOf(searchInput);
+      const index = student.indexOf(filterText);
       return index === -1 ? (
         <p>{student}</p>
       ) : (
         <p>
           {student.slice(0, index)}
           <span style={{ backgroundColor: "yellow" }}>
-            {student.slice(index, index + searchInput.length)}
+            {student.slice(index, index + filterText.length)}
           </span>
-          {student.slice(index + searchInput.length)}
+          {student.slice(index + filterText.length)}
         </p>
       );
     });
-  }, [searchInput]);
-
-  console.log("data", data);
+  }, [filterText]);
 
   const handleSearchInputChange = (e) => {
     setSearchInput(e.target.value);
+    startTransition(() => {
+      setFilterText(e.target.value);
+    });
   };
 
   return (
@@ -36,13 +39,13 @@ const Form = () => {
       <h3>Form</h3>
       <label htmlFor={id}>Search Name: </label>
       <input
-        type="text"
-        name="name"
+        type='text'
+        name='name'
         id={id}
         value={searchInput}
         onChange={handleSearchInputChange}
       />
-      <StudentList data={data} />
+      {isPeding ? <p>Loading...</p> : <StudentList data={data} />}
     </>
   );
 };
