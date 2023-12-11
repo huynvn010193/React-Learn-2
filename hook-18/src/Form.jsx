1d159756b92f7f6e65bedfa8d0cca89d0354e990
huynvn010193 <hpn010193@gmail.com>
2023-12-11 09:01:17 +0700

    Pending code

--- hook-18/src/Form.jsx
+++ hook-18/src/Form.jsx
@@ -1,12 +1,32 @@
-import React, { useId, useState } from "react";
+import React, { useId, useMemo, useState } from "react";
 import StudentList from "./StudentList";
-import StudentListData from "./mocks/studentList.json";
+import studentListData from "./mocks/studentList.json";
 
 const Form = () => {
   // useId():
   const id = useId();
-  const data = StudentListData;
   const [searchInput, setSearchInput] = useState("");
+
+  // dùng useMemo để ghi nhớ lại giá trị của nó
+  const data = useMemo(() => {
+    return studentListData.map((student) => {
+      const index = student.indexOf(searchInput);
+      return index === -1 ? (
+        <p>{student}</p>
+      ) : (
+        <p>
+          {student.slice(0, index)}
+          <span style={{ backgroundColor: "yellow" }}>
+            {student.slice(index, index + searchInput.length)}
+          </span>
+          {student.slice(index + searchInput.length)}
+        </p>
+      );
+    });
+  }, [searchInput]);
+
+  console.log("data", data);
+
   const handleSearchInputChange = (e) => {
     setSearchInput(e.target.value);
   };
